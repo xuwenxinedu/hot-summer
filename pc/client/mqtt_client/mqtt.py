f18e93266fb3fc44f1a32e3507c5d0eff4b3b19e
luxuemin <826790670@qq.com>
2022-06-30 10:58:31 +0800

    update mqtt client

--- pc/client/mqtt_client/mqtt.py
+++ pc/client/mqtt_client/mqtt.py
@@ -1,8 +1,8 @@
-from sre_parse import State
+
 import paho.mqtt.client as mqtt
 import json
-from algorithm import identify as it
-
+from pc.client.algorithm import identify as it
+from queue import Queue
 class MQTT:
     def __init__(self):
         super().__init__()
@@ -12,13 +12,38 @@ class MQTT:
             self.port = setting['port']
             self.username = setting['client']
             self.password = setting['pwd']
-            self.topic = setting['topic']
+            self.topic_publish = setting['topic_publish']
+            self.topic_subscribe = setting['topic_subscribe']
 
         # MQTT设置
         client_id = '655cc02f46d44dc5b1a048e89a790ffe'
         self.client = mqtt.Client(client_id)
         self.connect_mqtt()
+        self.client.on_message = self.on_message
+        self.client.on_subscribe = self.on_subscribe
+        self.queue_rcv_msg = Queue(30)
 
+    # 消息回调函数
+    def on_message(self, client, userdata, msg):
+        topic = msg.topic
+        payload = str(json.loads(msg.payload))
+        print("主题：" + topic + "\n" + "消息：" + payload)
+        if not self.queue_rcv_msg.full():
+            self.queue_rcv_msg.put(payload)
+            json_payload = json.loads(msg.payload)
+            print(json_payload['Protocol30'])
+        else:
+            print("mqtt_client:queue_rcv_msg full!")
+            self.queue_rcv_msg.queue.clear()
+
+    # 订阅成功回
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        print("订阅成功: " + str(mid))
+
+
+    # 取消订阅成功回调函数
+    def on_unsubscribe(self, client, userdata, mid):
+        print("取消订阅成功：" + str(mid))
 
 
     # 连接服务器函数
@@ -30,6 +55,7 @@ class MQTT:
         except Exception:
             print("连接出错，原因:{}".format(str(Exception)))
             return
+        self.client.subscribe(self.topic_subscribe,qos=0)
         self.client.loop_start()
 
     # 断开服务器函数
@@ -50,11 +76,11 @@ class MQTT:
             }
 
         )
-        self.client.publish(self.topic, payload)
+        self.client.publish(self.topic_publish, payload)
         
     
     def publish(self, payload):
-        self.client.publish(self.topic, payload)
+        self.client.publish(self.topic_publish, payload)
 
     def reset(self):
         payload = json.dumps({
@@ -99,5 +125,7 @@ class MQTT:
             }
         )
         self.publish(payload)
+
+
         
 
